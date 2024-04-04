e921e876c92ae51dc96a06c1a69c74fa32ec6c0f
LLLLLL <lina.stawi@hotmail.de>
2024-04-04 21:27:20 +0200

    final

---
+++ gg.java
@@ -0,0 +1,10 @@
+public class gg {
+    int array [] = {82, 48,44, 62, 8, 1, 6};
+    int temp;
+
+    for (int i = 0; i< array.length; i++){
+        for (j=0; j>0; j--){
+
+        }
+    }
+}
---
+++ interaction.js
@@ -0,0 +1,5 @@
+let head = document.getElementById("items");
+
+if (head.click()== true) {
+    head.backgroundColor = "rgb(80,80,0)";
+}
